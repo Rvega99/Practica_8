bf2701970a5f76a502a38118d9dc58c57f690b83
Rvega99 <rdelavega99@gmail.com>
2023-01-17 14:20:54 -0600

    Practica_8

---
+++ gcc/GPIO.d
@@ -0,0 +1,35 @@
+gcc/GPIO.o: GPIO.c lib/include.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\stdint.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\_default_types.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\features.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\_newlib_version.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_intsup.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_stdint.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\stdlib.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\ieeefp.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\_ansi.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\newlib.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\config.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\reent.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\_ansi.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_types.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\_types.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\lock.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\cdefs.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\stdlib.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\string.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\string.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\math.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\stdio.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdarg.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\types.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\_pthreadtypes.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\machine\types.h \
+ c:\users\rvega\documents\micro\gcc-arm-none-eabi-10.3-2021.10\arm-none-eabi\include\sys\stdio.h \
+ lib/TM4C123GH6PM.h lib/../CMSIS/Include/core_cm4.h \
+ lib/../CMSIS/Include/core_cmInstr.h lib/../CMSIS/Include/core_cmFunc.h \
+ lib/../CMSIS/Include/core_cm4_simd.h lib/../lib/system_TM4C123.h \
+ lib/GPIO.h lib/PLL.h lib/UART.h
